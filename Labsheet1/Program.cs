4b52e3948342ead8bd2a42c4c5315e6a456dc906
cnv2003 <chalanika03@gmail.com>
2024-05-06 19:08:58 +0530

    Add project files.

---
+++ Labsheet1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the rectangle:");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = Area(length, width);
+
+            Console.WriteLine("The area is: " + area);
+            Console.ReadLine();
+        }
+        static double Area(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
+
+        
